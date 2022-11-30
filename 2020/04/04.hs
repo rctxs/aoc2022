080bf44e37f2c40ce27a6512b53b92f172eb5675
Raphael Taxis <raphael.taxis@fu-berlin.de>
2022-12-01 00:21:46 +0100

    2020 exercise

--- 2020/04/04.hs
+++ 2020/04/04.hs
@@ -5,16 +5,13 @@ main = do
   f1 <- readFile "input01.txt"
   -- f2 <- readFile "input02.txt"
   print $ taskOne f1
+  print $ taskTwo f1
 
---print $ taskTwo f2
+taskOne :: String -> Integer
+taskOne = sum . map (parseBool . completePassport . splitPassportStringToTuples) . groupLinesToPassportStrings . lines
 
---taskOne :: String -> Integer
-taskOne = sum . map (parseBool . validatePassport . splitPassportStringToTuples) . groupLinesToPassportStrings . lines
-
---taskOne = map (validatePassport . splitPassportStringToTuples) . groupLinesToPassportStrings . lines
-
---taskTwo :: String -> Maybe String
---taskTwo _ = Nothing
+taskTwo :: String -> Integer
+taskTwo = sum . map (parseBool . completePassport' . splitPassportStringToTuples) . groupLinesToPassportStrings . lines
 
 parseBool :: Bool -> Integer
 parseBool b = if b then 1 else 0
@@ -38,8 +35,8 @@ splitPassportStringToTuples = map (makeTuple . splitOn ":") . splitOn " "
   where
     makeTuple xs = (head xs, last xs)
 
-validatePassport :: [(String, String)] -> Bool
-validatePassport xs = complete xs || oneMissingField xs
+completePassport :: [(String, String)] -> Bool
+completePassport xs = complete xs || oneMissingField xs
   where
     complete = (== 8) . length
     oneMissingField xs = length xs == 7 && containsCid xs
@@ -47,3 +44,23 @@ validatePassport xs = complete xs || oneMissingField xs
     containsCid (x : xs)
       | fst x == "cid" = False
       | otherwise = containsCid xs
+
+completePassport' :: [(String, String)] -> Bool
+completePassport' xs = completePassport xs && all validField xs
+
+validField :: (String, String) -> Bool
+validField ("byr", value) = 1920 <= year && year <= 2002 where year = read value
+validField ("iyr", value) = 2010 <= year && year <= 2020 where year = read value
+validField ("eyr", value) = 2020 <= year && year <= 2030 where year = read value
+validField ("hgt", value)
+  | length value == 5 && value !! 3 == 'c' = 150 <= heightCm && heightCm <= 193
+  | length value == 4 && value !! 2 == 'i' = 59 <= heightIn && heightIn <= 76
+  | otherwise = False
+  where
+    heightCm = read (take 3 value)
+    heightIn = read (take 2 value)
+validField ("hcl", value) = head value == '#' && all (`elem` (['a' .. 'f'] ++ ['0' .. '9'])) (tail value)
+validField ("ecl", value) = value `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+validField ("pid", value) = length value == 9 && all (`elem` ['0' .. '9']) value
+validField ("cid", _) = True
+validField _ = False
