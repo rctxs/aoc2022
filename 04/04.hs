dddfcc9137f18706bae98dc74db763b4a8a567d4
Raphael Taxis <raphael.taxis@fu-berlin.de>
2022-12-05 17:57:51 +0100

    formating

--- 04/04.hs
+++ 04/04.hs
@@ -1,18 +1,27 @@
 import Data.List.Split
 
+main :: IO ()
 main = do
   f1 <- readFile "input01.txt"
   print $ taskOne f1
   print $ taskTwo f1
 
-taskOne = length . filter overlap . map (parseLine) . lines
-taskTwo = length . filter overlap' . map (parseLine) . lines
+taskOne :: String -> Int
+taskOne = length . filter overlap . map parseLine . lines
 
+taskTwo :: String -> Int
+taskTwo = length . filter overlap' . map parseLine . lines
+
+overlap :: ((Int, Int), (Int, Int)) -> Bool
 overlap ((a, b), (a', b')) = a <= a' && b' <= b || a' <= a && b <= b'
-overlap' ((a, b), (a', b')) = 0 /= (length [e | e<-[a..b], e'<-[a'..b'], e==e'])
 
+overlap' :: ((Int, Int), (Int, Int)) -> Bool
+overlap' ((a, b), (a', b')) = not (null ([e | e <- [a .. b], e' <- [a' .. b'], e == e']))
+
+parseLine :: [Char] -> ((Int, Int), (Int, Int))
 parseLine line = ((a, b), (a', b'))
-    xline = ((map (map (\x -> read x :: Int))) . map (splitOn "-") . splitOn ",") line
+  where
+    xline = (map (map (\x -> read x :: Int) . splitOn "-") . splitOn ",") line
     a = (head . head) xline
     b = (last . head) xline
     a' = (head . last) xline
